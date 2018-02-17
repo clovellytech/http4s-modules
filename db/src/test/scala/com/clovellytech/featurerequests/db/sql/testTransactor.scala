b36e39dfa176d717670dd1ddd4a4de37da458145
Zak Patterson <pattersonzak@gmail.com>
2018-02-17 11:11:32 -0500

    Fix error in implicit requirements, Effect and Async

--- db/src/test/scala/com/clovellytech/featurerequests/db/sql/testTransactor.scala
+++ db/src/test/scala/com/clovellytech/featurerequests/db/sql/testTransactor.scala
@@ -1,17 +1,16 @@
 package com.clovellytech.featurerequests.db
 package sql
 
-import cats.effect.{Async, IO}
+import cats.effect.{Async, IO, Effect}
+import cats.implicits._
 import config.{DatabaseConfig, loadConfig}
 import doobie.util.transactor.Transactor
 
 object testTransactor {
-  def configTransactor[F[_]: Async] : F[Transactor[F]] = {
-    val t: F[Transactor[F]] = for {
-      cfg <- loadConfig[F, DatabaseConfig]("db")
-      tr <- getTransactor[F](cfg)
-    } yield tr
-  }
+  def configTransactor[F[_]: Async : Effect] : F[Transactor[F]] = for {
+    cfg <- loadConfig[F, DatabaseConfig]("db")
+    tr <- getTransactor[F](cfg)
+  } yield tr
 
   def getTestTransactor : IO[Transactor[IO]] = configTransactor[IO]
 
