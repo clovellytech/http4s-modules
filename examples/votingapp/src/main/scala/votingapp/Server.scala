4e5d0f00d4c506a4bb0e586b15f52e06c94481c8
Zak Patterson <pattersonzak@gmail.com>
2018-05-02 14:33:14 -0400

    Fix version refactor example app (#19)
    * Bump version
    
    * Fix example app to use easy endpoint constructors

--- examples/votingapp/src/main/scala/votingapp/Server.scala
+++ examples/votingapp/src/main/scala/votingapp/Server.scala
@@ -1,21 +1,13 @@
 package votingapp
 
-import com.clovellytech.featurerequests._
-
 import cats.effect._
 import cats.implicits._
-import com.clovellytech.db.config._
-import com.clovellytech.auth.domain.tokens.TokenService
-import com.clovellytech.auth.domain.users.UserService
 import com.clovellytech.auth.infrastructure.endpoint.AuthEndpoints
-import com.clovellytech.auth.infrastructure.repository.persistent.{TokenRepositoryInterpreter, UserRepositoryInterpreter}
+import com.clovellytech.db.config._
+import com.clovellytech.featurerequests.infrastructure.endpoint.{RequestEndpoints, VoteEndpoints}
 import fs2.StreamApp.ExitCode
 import fs2.{Stream, StreamApp}
 import org.http4s.server.blaze.BlazeBuilder
-import domain.votes.VoteService
-import domain.requests.RequestService
-import infrastructure.repository.persistent.{RequestRepositoryInterpreter, VoteRepositoryInterpreter}
-import infrastructure.endpoint._
 import tsec.passwordhashers.jca.BCrypt
 
 import scala.concurrent.ExecutionContext
@@ -34,18 +26,10 @@ object Server extends StreamApp[IO] {
       conf <- Stream.eval(loadConfig[F, DatabaseConfig]("db"))
       xa <- Stream.eval(conf.dbTransactor[F])
       _ <- Stream.eval(com.clovellytech.featurerequests.db.initializeAll(xa))
-      voteRepo = new VoteRepositoryInterpreter(xa)
-      requestRepo = new RequestRepositoryInterpreter(xa)
-      userRepo = new UserRepositoryInterpreter(xa)
-      tokenRepo = new TokenRepositoryInterpreter(xa)
-      userService = new UserService(userRepo)
-      tokenService = new TokenService(tokenRepo)
-      voteService = new VoteService(voteRepo)
-      requestService = new RequestService(requestRepo)
-      authEndpoints = new AuthEndpoints(userService, tokenService, BCrypt)
-      authService = authEndpoints.Auth
-      requestEndpoints = new RequestEndpoints(requestService)
-      voteEndpoints = new VoteEndpoints(voteService)
+      authEndpoints = AuthEndpoints.persistingEndpoints(xa, BCrypt)
+      authService =  authEndpoints.Auth
+      requestEndpoints =  RequestEndpoints.persistingEndpoints(xa)
+      voteEndpoints =  VoteEndpoints.persistingEndpoints(xa)
       exitCode <- BlazeBuilder[F]
         .bindHttp(8080, "localhost")
         .mountService(authEndpoints.endpoints, "/auth/")
--- project/Version.scala
+++ project/Version.scala
@@ -1,4 +1,4 @@
 object Version{
-  val version = "0.0.2-SNAPSHOT"
+  val version = "0.0.3-SNAPSHOT"
   val scalaVersion = "2.12.5"
 }
