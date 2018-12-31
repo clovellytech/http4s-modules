19e23d45cb12f27dd3f5961b2cd67ace2ffedd3e
Zak Patterson <pattersonzak@gmail.com>
2018-12-30 23:21:01 -0500

    Add initial travis build

---
+++ script/travis_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+psql -c 'create database ct_auth_test;' -U postgres 
+psql -c 'create database ct_permissions_test;' -U postgres 
+psql -c 'create database ct_files_test;' -U postgres 
+psql -c 'create database ct_feature_requests_test;' -U postgres  
+
+git clone https://github.com/zakpatterson/tsec
+cd tsec/
+sbt publishLocal
+cd ..
