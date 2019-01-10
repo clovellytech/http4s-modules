5b35068f6c06c7381de9df4e29e2a8ab1be6ccc1
Zak Patterson <pattersonzak@gmail.com>
2019-01-10 12:31:49 -0500

    Add another pg database for travis ci testing

--- script/travis_init.sh
+++ script/travis_init.sh
@@ -4,6 +4,7 @@ psql -c 'create database ct_auth_test;' -U postgres
 psql -c 'create database ct_permissions_test;' -U postgres 
 psql -c 'create database ct_files_test;' -U postgres 
 psql -c 'create database ct_feature_requests_test;' -U postgres  
+psql -c 'create database h4sm_docs_gen' -U postgres
 
 git clone https://github.com/zakpatterson/http4s
 cd http4s
