d3f48745ec01927d56339cf8e58c44829313614e
Zak Patterson <pattersonzak@gmail.com>
2019-01-10 13:22:49 -0500

    Fix configuration setup in test

--- features/src/test/scala/h4sm/featurerequests/db/sql/testTransactor.scala
+++ features/src/test/scala/h4sm/featurerequests/db/sql/testTransactor.scala
@@ -2,12 +2,9 @@ package h4sm.featurerequests.db
 package sql
 
 import cats.effect.IO
-import cats.implicits._
-import com.typesafe.config.ConfigFactory
 import doobie.util.transactor.Transactor
-import h4sm.db.config.DatabaseConfig
+import h4sm.db.config._
 import h4sm.dbtesting.transactor.getInitializedTransactor
-import io.circe.config.syntax._
 import io.circe.generic.auto._
 import scala.concurrent.ExecutionContext
 
@@ -15,12 +12,8 @@ object testTransactor {
   implicit lazy val cs = IO.contextShift(ExecutionContext.Implicits.global)
 
   def getTransactor : IO[Transactor[IO]] =
-    ConfigFactory
-      .load()
-      .as[DatabaseConfig]("db")
-      .leftWiden[Throwable]
-      .raiseOrPure[IO]
-      .flatMap(getInitializedTransactor(_, "ct_auth", "featurerequests"))
+    loadConfigF[IO, DatabaseConfig]("db")
+    .flatMap(getInitializedTransactor(_, "ct_auth", "ct_feature_requests"))
 
   lazy val testTransactor: Transactor[IO] = getTransactor.unsafeRunSync()
 }
