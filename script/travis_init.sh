684b20fce3bc2e93d4fba6c81aa9aa3450b7677d
Zak Patterson <pattersonzak@gmail.com>
2019-01-11 11:49:04 -0500

    Fix server local save location

--- script/travis_init.sh
+++ script/travis_init.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# For files
+mkdir /tmp/filestest
+
+
+# Test databases
 psql -c 'create database ct_auth_test;' -U postgres 
 psql -c 'create database ct_permissions_test;' -U postgres 
 psql -c 'create database ct_files_test;' -U postgres 
 psql -c 'create database ct_feature_requests_test;' -U postgres  
 psql -c 'create database h4sm_docs_gen' -U postgres
 
+
+# Modules we have to publish locally because we're 
+# waiting on releases from them.
 git clone https://github.com/zakpatterson/http4s
 cd http4s
 git checkout v0.20.0-M5-ct
