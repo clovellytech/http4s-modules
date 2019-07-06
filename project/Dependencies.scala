c82d7b3a832b8f2e6b8f29b63372d4db4315860e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-07-06 22:09:42 +0200

    Update http4s-blaze-client, ... to 0.20.4 (#91)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object dependencies {
   val cryptobits = "1.1"
   val doobie = "0.7.0"
   val flyway = "5.2.4"
-  val http4s = "0.20.3"
+  val http4s = "0.20.4"
   val logback = "1.2.3"
   val postgres = "42.2.6"
   val scalaCheck = "1.14.0"
