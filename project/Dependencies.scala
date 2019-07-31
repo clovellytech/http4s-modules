ccad1ace6087fe79c035c3e9a10e2debf72234e0
Scala Steward <me@scala-steward.org>
2019-07-31 06:04:59 +0200

    Update better-monadic-for to 0.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object dependencies {
   val compilerPlugins = Seq(
     addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.10"),
     addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full),
-    addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.0")
+    addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
   )
 
   val apacheLang3 = "3.9"
