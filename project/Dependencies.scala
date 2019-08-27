7595462f32ac30f7285afae0fb32f7b6a1b32b6e
Scala Steward <me@scala-steward.org>
2019-08-27 17:05:51 +0200

    Update circe-core, circe-generic, ... to 0.12.0-RC4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object dependencies {
   val cats = "2.0.0-RC1"
   val catsMtl = "0.6.0"
   val catsEffect = "2.0.0-RC1"
-  val circe = "0.12.0-RC3"
+  val circe = "0.12.0-RC4"
   val circeConfig = "0.7.0-M1"
   val cryptobits = "1.1"
   val doobie = "0.8.0-RC1"
