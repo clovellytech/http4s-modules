a01250b9a6485caf013a44f97efd1aeb0396dc96
jules Ivanic <jules.ivanic@gmail.com>
2019-11-11 10:07:42 +1100

    Fix compilation

--- modules/auth/comm/js/src/main/scala/UserDetail.scala
+++
@@ -1,6 +0,0 @@
-package h4sm.auth.comm
-
-final case class UserDetail(
-    username: String,
-    joinTime: String,
-)
--- modules/auth/comm/js/src/main/scala/UserDetailCodecs.scala
+++
@@ -1,9 +0,0 @@
-package h4sm.auth.comm
-
-import io.circe.{Decoder, Encoder}
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
-
-trait UserDetailCodecs {
-  implicit val userDetailDecoder: Decoder[UserDetail] = deriveDecoder
-  implicit val userDetailEncoder: Encoder[UserDetail] = deriveEncoder
-}
