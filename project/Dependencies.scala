105c658a26d7a0889887fd4477fa495e0c095273
Scala Steward <me@scala-steward.org>
2020-02-06 20:09:40 +0100

    Update flyway-core to 6.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object dependencies {
     val circeConfig = "0.7.0"
     val cryptobits = "1.1"
     val doobie = "0.8.8"
-    val flyway = "6.2.1"
+    val flyway = "6.2.2"
     val http4s = "0.21.0-RC4"
     val janino = "3.1.0"
     val kindProjector212 = "0.10.3"
