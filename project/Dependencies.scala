2cfe5626d428914dda69ab9a98e4a49b56104039
Scala Steward <me@scala-steward.org>
2020-02-08 00:20:15 +0100

    Update cats-effect to 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object dependencies {
     val betterMonadicFor = "0.3.1"
     val cats = "2.1.0"
     val catsMtl = "0.7.0"
-    val catsEffect = "2.1.0"
+    val catsEffect = "2.1.1"
     val circe = "0.12.3"
     val circeConfig = "0.7.0"
     val cryptobits = "1.1"
