d60dd800d62a437242c57c40cc13df57ab86c2d1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-07-16 16:43:56 +0200

    Update commons-lang3 to 3.11 (#328)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object dependencies {
   )
 
   object versions {
-    val apacheLang3 = "3.10"
+    val apacheLang3 = "3.11"
     val bcrypt = "3.1"
     val betterMonadicFor = "0.3.1"
     val cats = "2.1.1"
