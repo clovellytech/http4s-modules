c3e61c550fdca26da8afe61dbd1ef634c775eb00
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-09 08:40:34 +0200

    Update http4s-blaze-client, ... to 0.21.7 (#338)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object dependencies {
     val cryptobits = "1.1"
     val doobie = "0.9.0"
     val flyway = "6.5.3"
-    val http4s = "0.21.6"
+    val http4s = "0.21.7"
     val janino = "3.1.2"
     val kindProjector212 = "0.10.3"
     val kindProjector213 = "0.11.0"
