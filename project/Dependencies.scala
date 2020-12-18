ff43fe50a1426ae688f08227c9324728fcb5981d
Scala Steward <me@scala-steward.org>
2020-12-18 22:29:13 +0100

    Update cats-effect to 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object dependencies {
     val cats = "2.1.1"
     val catsScalacheck = "0.2.0.1"
     val catsMtl = "0.7.1"
-    val catsEffect = "2.3.0"
+    val catsEffect = "2.3.1"
     val circe = "0.13.0"
     val circeConfig = "0.8.0"
     val cryptobits = "1.1"
