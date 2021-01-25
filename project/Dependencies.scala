bce5cef88dc849bb902c823f71f99f7284954473
Scala Steward <me@scala-steward.org>
2021-01-25 16:18:12 +0100

    Update janino to 3.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object dependencies {
     val doobie = "0.9.4"
     val flyway = "7.3.2"
     val http4s = "0.21.14"
-    val janino = "3.1.2"
+    val janino = "3.1.3"
     val kindProjector212 = "0.10.3"
     val kindProjector213 = "0.11.0"
     val logback = "1.2.3"
