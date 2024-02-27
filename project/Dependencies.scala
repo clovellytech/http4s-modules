4436d023464b4d8b521d69efe3f214d0a632b429
Scala Steward <scala_steward@virtuslab.com>
2024-02-27 16:40:29 +0000

    Update scala-library to 2.12.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,23 +7,22 @@ import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin
 import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin.autoImport._
 
-
 object dependencies {
 
+  lazy val copyFastOptJS =
+    TaskKey[Unit]("copyFastOptJS", "Copy javascript files to target directory")
 
-  lazy val copyFastOptJS = TaskKey[Unit]("copyFastOptJS", "Copy javascript files to target directory")
+  val withTests: String = "compile->compile;test->test"
+  val inTestOnly: String = "test->test"
 
-  val withTests : String = "compile->compile;test->test"
-  val inTestOnly : String = "test->test"
-
-  val scala212 = "2.12.12"
+  val scala212 = "2.12.19"
   val scala213 = "2.13.3"
 
   lazy val JsTest = config("js").extend(Test)
   lazy val JvmTest = config("jvm").extend(Test)
 
   val addResolvers = Seq(
-    Resolver.sonatypeRepo("public")
+    Resolver.sonatypeRepo("public"),
   )
 
   object versions {
@@ -56,19 +55,22 @@ object dependencies {
   }
 
   def compilerPlugins = Seq(
-    compilerPlugin("com.olegpy" %% "better-monadic-for" % versions.betterMonadicFor)
+    compilerPlugin("com.olegpy" %% "better-monadic-for" % versions.betterMonadicFor),
   )
 
   def compilerPluginsForVersion(version: String) =
     CrossVersion.partialVersion(version) match {
       case Some((2, major)) if major < 13 =>
         compilerPlugins ++ Seq(
-          compilerPlugin("org.scalamacros" % "paradise" % versions.macroParadise cross CrossVersion.full),
-          compilerPlugin("org.typelevel" %% "kind-projector" % versions.kindProjector212)
+          compilerPlugin(
+            ("org.scalamacros" % "paradise" % versions.macroParadise).cross(CrossVersion.full),
+          ),
+          compilerPlugin("org.typelevel" %% "kind-projector" % versions.kindProjector212),
         )
-      case Some((2, major)) if major == 13 => compilerPlugins ++ Seq(
-        compilerPlugin("org.typelevel" % s"kind-projector_$version" % versions.kindProjector213)
-      )
+      case Some((2, major)) if major == 13 =>
+        compilerPlugins ++ Seq(
+          compilerPlugin("org.typelevel" % s"kind-projector_$version" % versions.kindProjector213),
+        )
       case _ => compilerPlugins
     }
 
@@ -77,7 +79,7 @@ object dependencies {
     "http4s-blaze-server",
     "http4s-blaze-client",
     "http4s-circe",
-    "http4s-dsl"
+    "http4s-dsl",
   ).map("org.http4s" %% _ % versions.http4s)
 
   val testDeps = Seq(
@@ -92,11 +94,11 @@ object dependencies {
 
   val dbDeps = Seq(
     "org.flywaydb" % "flyway-core" % versions.flyway,
-    "org.postgresql" % "postgresql" % versions.postgres
+    "org.postgresql" % "postgresql" % versions.postgres,
   ) ++ Seq(
     "doobie-core",
     "doobie-postgres",
-    "doobie-hikari"
+    "doobie-hikari",
   ).map("org.tpolecat" %% _ % versions.doobie)
 
   val commonDeps = Seq(
@@ -105,7 +107,7 @@ object dependencies {
     "cats-mtl-core" -> versions.catsMtl,
   ).map(("org.typelevel" %% (_: String) % (_: String)).tupled) ++ Seq(
     "org.apache.commons" % "commons-lang3" % versions.apacheLang3,
-    "ch.qos.logback" %  "logback-classic" % versions.logback,
+    "ch.qos.logback" % "logback-classic" % versions.logback,
     "org.codehaus.janino" % "janino" % versions.janino,
     "org.typelevel" %% "simulacrum" % versions.simulacrum,
   ) ++ Seq(
@@ -114,7 +116,7 @@ object dependencies {
     "circe-parser",
 //    "circe-java8"
   ).map("io.circe" %% _ % versions.circe) ++ Seq(
-    "io.circe" %% "circe-config" % versions.circeConfig
+    "io.circe" %% "circe-config" % versions.circeConfig,
   )
 
   val authDeps = Seq(
@@ -126,6 +128,6 @@ object dependencies {
     "tsec-hash-jca",
     "tsec-jwt-mac",
     "tsec-jwt-sig",
-    "tsec-http4s"
+    "tsec-http4s",
   ).map("com.clovellytech" %% _ % versions.tsec)
 }
